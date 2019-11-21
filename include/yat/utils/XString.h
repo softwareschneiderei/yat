8fa085c1d8e8d631a59d593c35ed202fd404203a
Stephane Poirier <stephane.poirier@synchrotron-soleil.fr>
2019-11-21 16:59:31 +0000

    class XString is now marked as deprecated

--- include/yat/utils/XString.h
+++ include/yat/utils/XString.h
@@ -15,11 +15,11 @@
 // see http://www.cs.wustl.edu/~schmidt/ACE.html for more about ACE
 //
 // The thread native implementation has been initially inspired by omniThread
-// - the threading support library that comes with omniORB. 
+// - the threading support library that comes with omniORB.
 // see http://omniorb.sourceforge.net/ for more about omniORB.
-// The YAT library is free software; you can redistribute it and/or modify it 
-// under the terms of the GNU General Public License as published by the Free 
-// Software Foundation; either version 2 of the License, or (at your option) 
+// The YAT library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
 // any later version.
 //
 // The YAT library is distributed in the hope that it will be useful,
@@ -27,7 +27,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
 // Public License for more details.
 //
-// See COPYING file for license details 
+// See COPYING file for license details
 //
 // Contact:
 //      Nicolas Leclercq
@@ -45,28 +45,16 @@
 //=============================================================================
 #include <yat/CommonHeader.h>
 
-namespace yat 
+namespace yat
 {
 
-// ============================================================================
-//! \class XString 
-//! \brief Generic numeric string class.
-//!
-//! This template class provides conversion functions for a generic \<_T\> "numeric"
-//! string.
-// ============================================================================
+//! \deprecated use yat::String methods instead
 template <typename _T>
 class XString
 {
-public:  
-  
-  //! \brief Converts string content to numeric type \<_T\>.
-  //! 
-  //! Returns the converted string in numeric type \<_T\>.
-  //! Should also work for any "istringstream::operator>>" supported type.
-  //! \param _s The string to convert.
-  //! \param _throw If set to true, throws an exception if conversion fails.
-  //! \exception SOFTWARE_ERROR Thrown if conversion fails.
+public:
+
+  //! \deprecated
   static _T to_num (const std::string& _s, bool _throw = true)
   {
     std::istringstream iss(_s.c_str());
@@ -80,7 +68,7 @@ public:
       if (_throw)
       {
         OSStream desc;
-        desc << "conversion from string to num failed [" 
+        desc << "conversion from string to num failed ["
              << _s
              << "]"
              << std::ends;
@@ -92,25 +80,21 @@ public:
     }
 
     return num_val;
-  } 
+  }
 
-  //! \brief Converts from type \<_T\> to std::string.
-  //!
-  //! \param _t The \<_T\> type value to convert.
-  //! \param _throw If set to true, throws an exception if conversion fails.
-  //! \exception SOFTWARE_ERROR Thrown if conversion fails.
+  //! \deprecated
   static std::string to_string (const _T & _t, bool _throw = true)
   {
     std::ostringstream oss;
 
     oss << std::fixed << _t;
-    
+
     if ( oss.fail() )
     {
       if (_throw)
       {
         OSStream desc;
-        desc << "conversion from num to string failed [" 
+        desc << "conversion from num to string failed ["
              << _t
              << "]"
              << std::ends;
@@ -122,7 +106,7 @@ public:
     }
 
     return oss.str();
-  } 
+  }
 
 };
 
