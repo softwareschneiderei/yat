819bc683df8b6e44f23d3f842838b4afd71a5395
Stephane Poirier <stephane.poirier@synchrotron-soleil.fr>
2020-04-24 07:54:41 +0000

    Switch YAT_TRACE_THREADSAFE no longer effective

--- include/yat/LogHelper.h
+++ include/yat/LogHelper.h
@@ -15,11 +15,11 @@
 // see http://www.cs.wustl.edu/~schmidt/ACE.html for more about ACE
 //
 // The thread native implementation has been initially inspired by omniThread
-// - the threading support library that comes with omniORB. 
+// - the threading support library that comes with omniORB.
 // see http://omniorb.sourceforge.net/ for more about omniORB.
-// The YAT library is free software; you can redistribute it and/or modify it 
-// under the terms of the GNU General Public License as published by the Free 
-// Software Foundation; either version 2 of the License, or (at your option) 
+// The YAT library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
 // any later version.
 //
 // The YAT library is distributed in the hope that it will be useful,
@@ -27,7 +27,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
 // Public License for more details.
 //
-// See COPYING file for license details 
+// See COPYING file for license details
 //
 // Contact:
 //      Nicolas Leclercq
@@ -40,25 +40,9 @@
 #ifndef _YAT_LOGHELPER_H_
 #define _YAT_LOGHELPER_H_
 
-#if defined (YAT_TRACE_THREADSAFE)
-  #include <yat/threading/Mutex.h>
-  #define DECLARE_YAT_TRACE_MUTEX static yat::Mutex s_mtx
-  #define INSTANTIATE_YAT_TRACE_MUTEX yat::Mutex yat::TraceHelper::s_mtx
-  #define YAT_TRACE_LOCK  yat::AutoMutex<yat::Mutex> __yat_lock_trace_log(yat::TraceHelper::s_mtx)
-#else
-  #define YAT_TRACE_LOCK
-  #define DECLARE_YAT_TRACE_MUTEX
-  #define INSTANTIATE_YAT_TRACE_MUTEX
-#endif
-
 /*!
  *  Define the YAT_LOG & YAT_TRACE helper macro depending on the YAT_ENABLE_LOG macro
  *  These macros can be used by yat users to log in their applications
- *  
- *  When the YAT_TRACE_THREADSAFE switch is set, then a global mutex is used to avoid 
- *  trace overlaps when several threads use YAT_TRACE & YAT_LOG at the same time
- *  The global mutex must be initialized by the client code using INSTANTIATE_YAT_TRACE_MUTEX 
- *  macro somewhere in a cpp source file
  */
 #if defined (YAT_ENABLE_LOG)
 
@@ -73,34 +57,31 @@
       public:
         TraceHelper(const char* _func_name, const void * _this = 0 )
           :  instance(_this), func_name(_func_name)
-        { 
-          YAT_TRACE_LOCK;
-          std::cout << func_name 
-                    << " [this::" 
-                    << std::hex 
-                    << instance 
-                    << std::dec 
-                    << "] <-" 
-                    << std::endl; 
+        {
+          std::cout << func_name
+                    << " [this::"
+                    << std::hex
+                    << instance
+                    << std::dec
+                    << "] <-"
+                    << std::endl;
         };
 
         ~TraceHelper()
-        { 
-          YAT_TRACE_LOCK;
-          std::cout << func_name 
-                    << " [this::" 
-                    << std::hex 
+        {
+          std::cout << func_name
+                    << " [this::"
+                    << std::hex
                     << instance
-                    << std::dec 
-                    << "] ->" 
-                    << std::endl; 
+                    << std::dec
+                    << "] ->"
+                    << std::endl;
         };
 
       private:
         const void * instance;
         const char* func_name;
       public:
-        DECLARE_YAT_TRACE_MUTEX;
       };
     }
 #   define YAT_TRACE(func_name) \
@@ -115,7 +96,6 @@
 #  define YAT_LOG(s) \
     do \
     { \
-      YAT_TRACE_LOCK; \
       std::cout << "[this:" \
                  << std::hex \
                  << (void *)this \
@@ -126,14 +106,13 @@
                  << s \
                  << std::endl; \
     } while (0)
-    
+
 #  define YAT_LOG_STATIC(s) \
     do \
     { \
-      YAT_TRACE_LOCK; \
       std::cout << s << std::endl; \
     } while(0)
-    
+
 #else
 
 # define YAT_LOG(x)
@@ -145,3 +124,4 @@
 
 
 #endif
+
